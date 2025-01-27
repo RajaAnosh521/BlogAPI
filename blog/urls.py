917dd7c96fffdcdd941c7273bc31c1649f105b48
RajaAnosh521 <javariaali070@gmail.com>
2025-01-27 22:15:16 +0500

    "Setup staic files"

--- blog/urls.py
+++ blog/urls.py
@@ -26,4 +26,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')), 
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
