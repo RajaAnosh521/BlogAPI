93e97ee3963a831c1ecef596e388d20da5ffca63
RajaAnosh521 <javariaali070@gmail.com>
2025-01-29 20:28:25 +0500

    "adding redoc path"

--- blog/urls.py
+++ blog/urls.py
@@ -27,7 +27,9 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')), 
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
+   path('admin/', admin.site.urls),
+   path('api/', include('api.urls')), 
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+   
+   ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
